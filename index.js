f9e5c63f25d55b27d0e1561d214aa5df2a2719ed
Cecilia <109989926+Eon4@users.noreply.github.com>
2024-01-25 12:33:48 +0100

    update-working

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 import http from 'http';
+import  express  from 'express';
+import dotenv from 'dotenv';
+import db from './db.config.js'
 
-http.createServer((req, res) => {
+const app = express()
 
-    console.log('Hello World');
 
-}).listen(8081)
+app.get("/api", (req, res) => {
+db.query(`SELECT id,title FROM song`, (error, result) => {
+	if(error) {
+		console.error(error);
+	} else {
+        res.json(result);
+	}
+  });
+});
+
+
+dotenv.config();
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is working, Server started on port:" + process.env.PORT);
+});
+
+app.use((req, res) => {
+    res.status(404).send("Page not found")
+})
